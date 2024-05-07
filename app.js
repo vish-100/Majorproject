419083686ba43559e6a3b95592bb65b73a1bc4bf
vish-100 <vishaldeora54@gmail.com>
2024-05-07 15:15:29 +0530

    Changed root route

--- app.js
+++ app.js
@@ -80,9 +80,9 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-/* app.get("/",(req,res)=>{
-    res.send("Hi, I am root");
-}); */
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+});
 app.use((req,res,next) =>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
